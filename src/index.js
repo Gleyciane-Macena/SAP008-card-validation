7bbd052eaacb4908c4b5fe94a3324aabe9bb4205
Gleyci-Macena <gleycianemc@gmail.com>
2022-08-05 10:40:41 -0300

    Alt Imagem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
 
-console.log(validator);
+let ccNumber;
+document.getElementById("ok").addEventListener("click",take);
+
+function take() {
+  ccNumber = document.getElementById("cNumber").value;
+  let numeroalert = validator.maskify(ccNumber);
+  if(ccNumber!=""){
+    
+      let valid = validator.isValid(ccNumber);
+      if(valid==true){
+        alert ("Cartão válido" + " número: " +  numeroalert)
+      }else{
+        alert ("Cartão invalido" + " número: " +  numeroalert)
+      }
+      return numeroalert;
+    }
+    }
+
+document.getElementById("ok").addEventListener("click",camuflar);
+
+function camuflar(){
+  ccNumber = document.getElementById("card").value;
+  alert(validator.maskify(ccNumber) );
+} 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,27 @@
-const validator = {
-  // ...
-};
+const validator= {
+  maskify,isValid
+}
 
-export default validator;
+function isValid (cNumber) {
+  var sum = 0;
+
+  while (cNumber > 0) {
+    var a = cNumber % 10;
+    cNumber = Math.floor(cNumber / 10);
+    
+    var b = (cNumber % 10) * 2;
+    cNumber = Math.floor(cNumber / 10);
+    
+    if (b > 9) {
+      b -= 9;
+    }
+    
+    sum += a + b;
+  }
+  
+  return sum % 10 == 0;
+}
+function maskify(ccNumber){
+  return ccNumber.replace(/.(?=.{4})/g, "#")
+}
+export default validator
